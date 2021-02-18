3c246c9fb70c736bb77c57b18133118b7ca9ed6e
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-02-19 09:14:19 +1000

    Improve npm hook with node_modules dir check

--- zscripts/npm_postinstall.bash
+++ zscripts/npm_postinstall.bash
@@ -2,6 +2,14 @@
 
 set -e
 
+HERE=$(pwd)
+WITHOUT_NODE_MODULES=(${HERE//node_modules/})
+
+if [[ "$HERE" != "$WITHOUT_NODE_MODULES" ]]; then
+    # inside node_modules => stop
+    exit 0
+fi
+
 if [[ -z $CODEBUILD_START_TIME ]]; then
     echo ">>> copy git pre-commit hook <<<"
     cp ./zscripts/git-pre-commit.sh ./.git/hooks/pre-commit
